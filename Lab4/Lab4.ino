2434307413b5034b331d714bd160486fa64e08c5
camilalemus <lem18272@uvg.edu.gt>
2021-03-27 21:46:19 -0600

    Semaforo

--- Lab4/Lab4.ino
+++ Lab4/Lab4.ino
@@ -4,19 +4,41 @@
 //**************************************************************************
 
 const byte LED_ROJO = RED_LED;
-const byte LED_AMARILLO = YELLOW_LED;
+//const byte LED_AMARILLO = YELLOW_LED;
 const byte LED_VERDE = GREEN_LED;
 
 volatile byte state = LOW;
 
+const int push_1 = PUSH1;
+const int push_2 = PUSH2;
+
 void setup() {
   pinMode(LED_ROJO, OUTPUT);
-  pinMode(LED_AMARILLO, OUTPUT);
+//  pinMode(LED_AMARILLO, OUTPUT);
   pinMode(LED_VERDE, OUTPUT);
-  pinMode(push_1, INPUT_PULLUP);
-  pinMode(push_2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(push_1), player_1, RISING);
-  attachInterrupt(digitalPinToInterrupt(push_2), player_2, RISING);
+  
+
+
+//  pinMode(LED1_J1, OUTPUT);
+//  pinMode(LED2_J1, OUTPUT);
+//  pinMode(LED3_J1, OUTPUT);
+//  pinMode(LED4_J1, OUTPUT);
+//  pinMode(LED5_J1, OUTPUT);
+//  pinMode(LED6_J1, OUTPUT);
+//  pinMode(LED7_J1, OUTPUT);
+//  pinMode(LED8_J1, OUTPUT);
+//
+//  pinMode(LED1_J2, OUTPUT);
+//  pinMode(LED2_J2, OUTPUT);
+//  pinMode(LED3_J2, OUTPUT);
+//  pinMode(LED4_J2, OUTPUT);
+//  pinMode(LED5_J2, OUTPUT);
+//  pinMode(LED6_J2, OUTPUT);
+//  pinMode(LED7_J2, OUTPUT);
+//  pinMode(LED8_J2, OUTPUT);
+
+//  attachInterrupt(digitalPinToInterrupt(push_1), player_1, RISING);
+//  attachInterrupt(digitalPinToInterrupt(push_2), player_2, RISING);
 }
 
 void loop() {
@@ -24,26 +46,27 @@ void loop() {
   if (digitalRead (push_1 == 1) || digitalRead (push_2 == 1)){
     semaforo();
     }
-  
-  digitalWrite(ledPin, state);
-}
-
-void player_1() {
-  state = !state;
-}
-
-void player_2() {
-  state = !state;
+//  
+//  digitalWrite(ledPin, state);
+//}
+//
+//void player_1() {
+//  state = !state;
+//}
+//
+//void player_2() {
+//  state = !state;
 }
 
 void semaforo() {
   LED_ROJO = HIGH;
-  delay(1000)
-  LED_ROJO = LOW;
-  LED_AMARILLO = HIGH;
-  delay(3000)
-  LED_AMARILLO = LOW;
+  LED_VERDE = LOW;
+  delay(1000);
+  LED_ROJO = HIGH;  // El RGB para amarillo es 0% azul, 100% rojo y 100% verde
   LED_VERDE = HIGH;
-  delay(3000)
+  delay(3000);
+  LED_ROJO = LOW;
+  LED_VERDE = HIGH;
+  delay(3000);
   LED_VERDE = LOW;
 }
